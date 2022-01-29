7b9d7061d8f62aa76ad9f972bc6b11ddd9297530
Prajan <prajanshrestha10gmail.com>
2022-01-29 18:14:48 +0545

    Adding bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 // import 'semantic-ui-css/semantic.min.css'
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
